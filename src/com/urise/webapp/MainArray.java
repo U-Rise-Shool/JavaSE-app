6374a7f24a01f7723f51b9e6056ead514f678ace
U-Rise <mail@u-rise.com>
2016-05-19 21:24:59 +0300

    Update ArrayStorage

--- src/com/urise/webapp/MainArray.java
+++ src/com/urise/webapp/MainArray.java
@@ -1,5 +1,8 @@
 package com.urise.webapp;
 
+import com.urise.webapp.model.Resume;
+import com.urise.webapp.storage.ArrayStorage;
+
 /**
  *
  * Test for com.urise.webapp.storage.ArrayStorage
@@ -9,11 +12,11 @@ public class MainArray {
 
     public static void main(String[] args) {
         Resume r1 = new Resume();
-        r1.uuid = "uuid1";
+        r1.setUuid("uuid1");
         Resume r2 = new Resume();
-        r2.uuid = "uuid2";
+        r2.setUuid("uuid2");
         Resume r3 = new Resume();
-        r3.uuid = "uuid3";
+        r3.setUuid("uuid3");
 
         System.out.println(r1==r2);
 
@@ -21,13 +24,13 @@ public class MainArray {
         arrayStorage.save(r2);
         arrayStorage.save(r3);
 
-        System.out.println("Get r1: " + arrayStorage.get(r1.uuid));
+        System.out.println("Get r1: " + arrayStorage.get(r1.getUuid()));
         System.out.println("Size: " + arrayStorage.size());
 
         System.out.println("Get dummy: " + arrayStorage.get("dummy"));
 
         printAll();
-        arrayStorage.delete(r1.uuid);
+        arrayStorage.delete(r1.getUuid());
         printAll();
         arrayStorage.clear();
         printAll();
--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -6,35 +6,50 @@ import com.urise.webapp.model.Resume;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
-    int size = 0;
+    private Resume[] storage = new Resume[10000];
+    private int size = 0;
 
-    void clear() {
+    public void clear() {
     }
 
-    void save(Resume r) {
+    public void update(Resume r) {
+        // TODO check if resume present
+        System.out.println("ERROR");
+
     }
 
-    Resume get(String uuid) {
+    public void save(Resume r) {
+        // TODO check if resume not present
+    }
+
+    public Resume get(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (uuid == storage[i].uuid) {
+            if (uuid == storage[i].getUuid()) {
                 return storage[i];
             }
         }
         return null;
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
+        // TODO check if resume present
+        for (int i = 0; i < size; i++) {
+            if (uuid == storage[i].getUuid()) {
+                storage[i] = storage[size - 1];
+                storage[i] = null;
+                size--;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         return new Resume[0];
     }
 
-    int size() {
-        return 0;
+    public int size() {
+        return size;
     }
 }

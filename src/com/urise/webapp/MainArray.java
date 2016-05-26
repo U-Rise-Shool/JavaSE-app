4bb5f02652970a8ebb92e013c9e4e8990a27c9d7
U-Rise <mail@u-rise.com>
2016-05-26 20:36:48 +0300

    Add static class members

--- src/com/urise/webapp/MainArray.java
+++ src/com/urise/webapp/MainArray.java
@@ -8,39 +8,39 @@ import com.urise.webapp.storage.ArrayStorage;
  * Test for com.urise.webapp.storage.ArrayStorage
  */
 public class MainArray {
-    static ArrayStorage arrayStorage = new ArrayStorage();
+    private final static ArrayStorage ARRAY_STORAGE = new ArrayStorage();
 
     public static void main(String[] args) {
-        Resume r1 = new Resume();
+        final Resume r1 = new Resume();
         r1.setUuid("uuid1");
-        Resume r2 = new Resume();
+        final Resume r2 = new Resume();
         r2.setUuid("uuid2");
-        Resume r3 = new Resume();
+        final Resume r3 = new Resume();
         r3.setUuid("uuid3");
 
         System.out.println(r1==r2);
 
-        arrayStorage.save(r1);
-        arrayStorage.save(r2);
-        arrayStorage.save(r3);
+        ARRAY_STORAGE.save(r1);
+        ARRAY_STORAGE.save(r2);
+        ARRAY_STORAGE.save(r3);
 
-        System.out.println("Get r1: " + arrayStorage.get(r1.getUuid()));
-        System.out.println("Size: " + arrayStorage.size());
+        System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.getUuid()));
+        System.out.println("Size: " + ARRAY_STORAGE.size());
 
-        System.out.println("Get dummy: " + arrayStorage.get("dummy"));
+        System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
 
         printAll();
-        arrayStorage.delete(r1.getUuid());
+        ARRAY_STORAGE.delete(r1.getUuid());
         printAll();
-        arrayStorage.clear();
+        ARRAY_STORAGE.clear();
         printAll();
 
-        System.out.println("Size: " + arrayStorage.size());
+        System.out.println("Size: " + ARRAY_STORAGE.size());
     }
 
     static void printAll() {
         System.out.println("\nGet All");
-        for (Resume r : arrayStorage.getAll()) {
+        for (Resume r : ARRAY_STORAGE.getAll()) {
             System.out.println(r);
         }
     }
--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -8,7 +8,9 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
+    private static final int STORAGE_LIMIT = 10000;
+
+    private Resume[] storage = new Resume[STORAGE_LIMIT];
     private int size = 0;
 
     public void clear() {
@@ -28,7 +30,7 @@ public class ArrayStorage {
     public void save(Resume r) {
         if (getIndex(r.getUuid()) != -1) {
             System.out.println("Resume " + r.getUuid() + " already exist");
-        } else if (size == storage.length) {
+        } else if (size == STORAGE_LIMIT) {
             System.out.println("Storage overflow");
         } else {
             storage[size] = r;

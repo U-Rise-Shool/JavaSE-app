666892cadb1bfd727eff98f7ae84c274ab98c3b4
U-Rise <mail@u-rise.com>
2016-05-26 20:23:19 +0300

    Add equals and hashCode

--- src/com/urise/webapp/model/Resume.java
+++ src/com/urise/webapp/model/Resume.java
@@ -12,6 +12,22 @@ public class Resume {
         return uuid;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Resume resume = (Resume) o;
+
+        return uuid.equals(resume.uuid);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return uuid.hashCode();
+    }
+
     public void setUuid(String uuid) {
         this.uuid = uuid;
     }

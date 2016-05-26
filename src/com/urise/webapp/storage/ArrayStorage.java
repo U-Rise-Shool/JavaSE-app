7d25c8cb10debfd886bd2a48ccb4b32c3c59d806
U-Rise <mail@u-rise.com>
2016-05-26 21:32:45 +0300

    Fix equals

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -54,7 +54,7 @@ public class ArrayStorage extends AbstractArrayStorage {
 
     protected int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (uuid == storage[i].getUuid()) {
+            if (uuid.equals(storage[i].getUuid())) {
                 return i;
             }
         }

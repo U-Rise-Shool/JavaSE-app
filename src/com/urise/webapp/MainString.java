0ec4fadf55d8c132105879d586b9baa07f473773
U-Rise <mail@u-rise.com>
2016-06-10 01:14:21 +0300

    String operation

---
+++ src/com/urise/webapp/MainString.java
@@ -0,0 +1,22 @@
+package com.urise.webapp;
+
+/**
+ * gkislin
+ * 10.06.2016
+ */
+public class MainString {
+    public static void main(String[] args) {
+        String[] strArray = new String[]{"1", "2", "3", "4", "5"};
+//        String result = "";
+        StringBuilder sb = new StringBuilder();
+        for (String str : strArray) {
+            sb.append(str).append(", ");
+        }
+        System.out.println(sb.toString());
+
+        String str1 = "abc";
+        String str3 = "c";
+        String str2 = ("ab" + str3).intern();
+        System.out.println(str1 == str2);
+    }
+}

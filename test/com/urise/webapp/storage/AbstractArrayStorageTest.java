20c3db8dec8bd46f76f8fd8496d6af59c26426ff
U-Rise <mail@u-rise.com>
2016-06-12 01:37:28 +0300

    Add main reflection and Junit tests

---
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -0,0 +1,67 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.exception.NotExistStorageException;
+import com.urise.webapp.model.Resume;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * gkislin
+ * 12.06.2016
+ */
+public abstract class AbstractArrayStorageTest {
+    private Storage storage;
+
+    private static final String UUID_1 = "uuid1";
+    private static final String UUID_2 = "uuid2";
+    private static final String UUID_3 = "uuid3";
+
+    @Before
+    public void setUp() throws Exception {
+        storage.clear();
+        storage.save(new Resume(UUID_1));
+        storage.save(new Resume(UUID_2));
+        storage.save(new Resume(UUID_3));
+    }
+
+    @Test
+    public void size() throws Exception {
+        Assert.assertEquals(3, storage.size());
+    }
+
+    @Test
+    public void clear() throws Exception {
+
+    }
+
+    @Test
+    public void update() throws Exception {
+
+    }
+
+    @Test
+    public void getAll() throws Exception {
+
+    }
+
+    @Test
+    public void save() throws Exception {
+
+    }
+
+    @Test
+    public void delete() throws Exception {
+
+    }
+
+    @Test
+    public void get() throws Exception {
+
+    }
+
+    @Test(expected = NotExistStorageException.class)
+    public void getNotExist() throws Exception {
+        storage.get("dummy");
+    }
+}
---
+++ test/com/urise/webapp/storage/ArrayStorageTest.java
@@ -0,0 +1,9 @@
+package com.urise.webapp.storage;
+
+/**
+ * gkislin
+ * 12.06.2016
+ */
+public class ArrayStorageTest extends AbstractArrayStorageTest {
+
+}

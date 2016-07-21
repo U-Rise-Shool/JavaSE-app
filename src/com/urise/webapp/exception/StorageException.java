b9518779d14f8008805ecc0b9a27a1ec4450e5fa
U-Rise <mail@u-rise.com>
2016-07-22 01:54:49 +0300

    8: HW8

--- src/com/urise/webapp/exception/StorageException.java
+++ src/com/urise/webapp/exception/StorageException.java
@@ -12,6 +12,12 @@ public class StorageException extends RuntimeException {
         this.uuid = uuid;
     }
 
+    public StorageException(String message, String uuid, Exception e) {
+        super(message, e);
+        this.uuid = uuid;
+    }
+
+
     public String getUuid() {
         return uuid;
     }

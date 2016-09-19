ecc068cb90b9aea64579c1c89aa5b0631ff76a46
U-Rise <mail@u-rise.com>
2016-09-19 21:29:42 +0300

    13_properties

---
+++ src/com/urise/webapp/Config.java
@@ -0,0 +1,36 @@
+package com.urise.webapp;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * gkislin
+ * 19.09.2016
+ */
+public class Config {
+    private static final File PROPS = new File("config\\resumes.properties");
+    private static final Config INSTANCE = new Config();
+
+    private Properties props = new Properties();
+    private File storageDir;
+
+    public static Config get() {
+        return INSTANCE;
+    }
+
+    private Config() {
+        try (InputStream is = new FileInputStream(PROPS)) {
+            props.load(is);
+            storageDir = new File(props.getProperty("storage.dir"));
+        } catch (IOException e) {
+            throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath());
+        }
+    }
+
+    public File getStorageDir() {
+        return storageDir;
+    }
+}
--- test/com/urise/webapp/storage/AbstractStorageTest.java
+++ test/com/urise/webapp/storage/AbstractStorageTest.java
@@ -1,5 +1,6 @@
 package com.urise.webapp.storage;
 
+import com.urise.webapp.Config;
 import com.urise.webapp.exception.ExistStorageException;
 import com.urise.webapp.exception.NotExistStorageException;
 import com.urise.webapp.model.*;
@@ -19,7 +20,7 @@ import static org.junit.Assert.assertTrue;
  * 12.06.2016
  */
 public abstract class AbstractStorageTest {
-    protected static final File STORAGE_DIR = new File("C:\\projects\\u-rise\\JavaSE-app\\storage");
+    protected static final File STORAGE_DIR = Config.get().getStorageDir();
 
     protected Storage storage;
 
